ea724b536d639fee81466993beafc26520383a6f
leonel argini <leonelargini@gmail.com>
2023-02-28 22:09:45 -0300

    una vez mas

---
+++ funciones.js
@@ -0,0 +1,6 @@
+
+
+document.getElementById('boton'). onclick = function () {
+    console.log("capturamos el evento")
+    document.getElementById('ejemplo').innerHTML="¡Hola! ¡bienvenido!, soy Leo. Tengo 38 años, y a continuacion podes saber mas sobre mi"
+}
